71c80ca944bbfe6f318af1634cd7c825442063b9
Diego Armando Cacilha <cacilha.tec@gmail.com>
2016-06-01 21:08:46 -0300

    testes feitos em casa. Descarta-los'

--- home.php
+++ home.php
@@ -11,6 +11,10 @@ and open the template in the editor.
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div id="placar">TODO write content</div>
+        <?php
+            echo 'Funcionou, Saporra!'
+            
+        ?>
+<!--        <div id="placar">TODO write content</div>-->
     </body>
 </html>
